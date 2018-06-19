626f88ff6667ac781013d389238201be5fe7fe35
Livia Vasconcelos <ltenoriovasconcelos@gmail.com>
2018-06-19 12:22:06 -0300

    feature/add-docs-to-library Add images to doc
    -
    
    Signed-off-by: Livia Vasconcelos <ltenoriovasconcelos@gmail.com>

--- sample/src/main/java/br/com/stant/libraries/stantuiandroid/workspace/WorkspaceActivity.java
+++ sample/src/main/java/br/com/stant/libraries/stantuiandroid/workspace/WorkspaceActivity.java
@@ -1,13 +1,22 @@
 package br.com.stant.libraries.stantuiandroid.workspace;
 
+import android.app.Dialog;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import br.com.stant.libraries.stantuiandroid.R;
 import br.com.stant.libraries.stantuiandroid.databinding.SampleWorkspaceActBinding;
+import br.com.stant.libraries.uilibrary.components.severitylevelindicatorview.SeverityLevelEnum;
+import br.com.stant.libraries.uilibrary.components.severitylevelindicatorview.SeverityLevelIndicatorView;
+import br.com.stant.libraries.uilibrary.databinding.ViewingUsersDialogBinding;
 
 /**
  * Created by denisvieira on 29/10/17.
@@ -16,34 +25,55 @@ import br.com.stant.libraries.stantuiandroid.databinding.SampleWorkspaceActBindi
 public class WorkspaceActivity extends AppCompatActivity {
 
     private  SampleWorkspaceActBinding mSampleWorkspaceActBinding;
+    private Dialog mViewingUsersDialog;
+    private ViewingUsersDialogBinding mViewingUsersDialogBinding;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mSampleWorkspaceActBinding = DataBindingUtil.setContentView(this, R.layout.sample_workspace_act);
-        mSampleWorkspaceActBinding.setExecutedPercent(50f);
-        mSampleWorkspaceActBinding.setSelectedPercent(25.5f);
-        mSampleWorkspaceActBinding.setButtonText("Button Text");
-        mSampleWorkspaceActBinding.setButtonShape(getResources().getDrawable(R.drawable.shape_round_blue));
-        mSampleWorkspaceActBinding.setButtonShadowShape(getResources().getDrawable(R.drawable.shape_round_dark_blue));
-        mSampleWorkspaceActBinding.buttonComponent.setOnClickListener(new View.OnClickListener() {
+        mSampleWorkspaceActBinding.teste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(WorkspaceActivity.this, "TESTE", Toast.LENGTH_SHORT).show();
+                dialogConfiguration();
+                mViewingUsersDialog.show();
             }
         });
 
 
-        mSampleWorkspaceActBinding.buttonComponent.setActiveStatusOnButton(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(WorkspaceActivity.this, "NOVO TESTE", Toast.LENGTH_SHORT).show();
-            }
-        });
 
-
-        mSampleWorkspaceActBinding.buttonComponent.setInactiveStatusOnButton();
+//        mSampleWorkspaceActBinding.setExecutedPercent(50f);
+//        mSampleWorkspaceActBinding.setSelectedPercent(25.5f);
+//        mSampleWorkspaceActBinding.setButtonText("Button Text");
+//        mSampleWorkspaceActBinding.setButtonShape(getResources().getDrawable(R.drawable.shape_round_blue));
+//        mSampleWorkspaceActBinding.setButtonShadowShape(getResources().getDrawable(R.drawable.shape_round_dark_blue));
+//
+//        mSampleWorkspaceActBinding .buttonComponent.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(WorkspaceActivity.this, "TESTE", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//
+//        mSampleWorkspaceActBinding.buttonComponent.setActiveStatusOnButton(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(WorkspaceActivity.this, "NOVO TESTE", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//
+//        mSampleWorkspaceActBinding.buttonComponent.setInactiveStatusOnButton();
 
     }
+
+    public void dialogConfiguration() {
+        mViewingUsersDialog = new Dialog(WorkspaceActivity.this);
+        mViewingUsersDialogBinding = DataBindingUtil.inflate(LayoutInflater.from(WorkspaceActivity.this), R.layout.viewing_users_dialog, null, false);
+
+    }
+
+
 }
---
+++ ui-library/src/main/java/br/com/stant/libraries/uilibrary/components/executedpercentbarview/doc/executedpercentbarview.md
@@ -0,0 +1,65 @@
+CHANGE!
+# Stant UI Android Library
+
+## Button Component
+> A custom view to action button with shadow.
+
+### Demo
+
+<p>
+  <img src="https://raw.githubusercontent.com/stantmob/stant-ui-android-library/master/ui-library/src/main/java/br/com/stant/libraries/uilibrary/components/buttoncomponent/doc/button_component.jpeg"  width="350">
+</p>
+
+### How to use
+
+1. Add Library with Readme setup .
+* [Back to Repository Readme](https://github.com/stantmob/stant-ui-android-library#how-add-into-your-project) .
+
+
+2. Add component to view file setting custom components (button text, shape and shadow shape) as you want.
+   Note that buttonShape and buttonShadowShape are drawable shapes.
+```xml
+
+     <br.com.stant.libraries.uilibrary.components.buttoncomponent.ButtonComponentView
+                android:id="@+id/button_component"
+                android:layout_width="match_parent"
+                app:buttonText="@{buttonText}"
+                app:buttonShape="@{buttonShape}"
+                app:buttonShadowShape="@{buttonShadowShape}"
+                android:layout_height="57dp"/>
+
+```
+
+3. Set action when on click button .
+```java
+
+    mBinding.buttonComponent.
+            setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //  put your actions here
+                }
+            });
+
+```
+
+4. Set Inactive State on Button
+
+```java
+
+    mSampleWorkspaceActBinding.buttonComponent.setInactiveStatusOnButton();
+
+```
+
+5. Returning to Active State on Button
+
+```java
+
+    mSampleWorkspaceActBinding.buttonComponent.setActiveStatusOnButton(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(WorkspaceActivity.this, "Actived Again", Toast.LENGTH_SHORT).show();
+        }
+    });
+
+```
