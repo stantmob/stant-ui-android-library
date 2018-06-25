0d2d648ea0e8f5b5b17278fa3f810cc8543a1ba6
Livia Vasconcelos <ltenoriovasconcelos@gmail.com>
2018-06-25 17:58:33 -0300

    feature/add-components-of-occurrence-app Add header component
    -
    
    Signed-off-by: Livia Vasconcelos <ltenoriovasconcelos@gmail.com>

---
+++ ui-library/src/main/java/br/com/stant/libraries/uilibrary/components/headerentitycardview/HeaderEntityCardViewContract.java
@@ -0,0 +1,15 @@
+package br.com.stant.libraries.uilibrary.components.headerentitycardview;
+
+/**
+ * Created by stant02 on 25/06/18.
+ */
+
+public interface HeaderEntityCardViewContract {
+
+    void setTitle(String title);
+    void setEndAt(String endAt);
+    void setStartAt(String startAt);
+    void setFirstQuantity(String firstQuantity);
+    void setSecondQuantity(String secondQuantity);
+
+}
